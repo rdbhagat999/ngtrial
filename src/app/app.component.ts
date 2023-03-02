cd11d620cfdd0d3ab55e138ee9542276a7734589
Ramandeep Bhagat <rdbhagat999@gmail.com>
2023-03-02 16:19:03 +0530

    adds clear button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: ` <main class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+  template: ` <main class="mx-auto max-w-5xl py-6 sm:px-6 lg:px-8">
     <div class="px-4 py-6 sm:px-0">
       <router-outlet></router-outlet>
     </div>
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -24,10 +24,18 @@ import { CardComponent } from "../card/card.component";
           <div class="w-full md:mt-52">
             <app-range-counter
               class="w-full space-y-8 lg:space-y-4"></app-range-counter>
+
+            <div class="flex justify-center md:justify-end mt-8">
+              <button
+                type="button"
+                class="w-[200px] text-white capitalize bg-green-800 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-2xl text-sm sm:w-auto px-5 py-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                Clear All
+              </button>
+            </div>
           </div>
         </div>
 
-        <div class="w-full mt-8 sm:mt-0">
+        <div class="w-full mt-8 sm:mt-0 md:w-1/2">
           <app-card
             *ngFor="let item of data"
             [item]="item"></app-card>
